3385b04eae459e4499b0cdb76a94ec2a997b3672
cmj1014 <cmj1014@163.com>
2019-08-14 15:28:15 +0800

    update vue.config

--- Online/vue.config.js
+++ Online/vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        rget: 'http://47.100.225.183:9000', //目标地址
+        target: 'http://47.100.225.183:9000', //目标地址
         //target: 'http://127.0.0.1:9000', //目标地址
         ws: true, //// 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
